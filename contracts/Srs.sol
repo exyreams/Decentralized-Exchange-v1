bb5e5f365c91547f0b7f38e4aec503e10fd9988b
exyreams <exyreams@gmail.com>
2023-03-03 16:28:56 +0545

    #up2.2

--- contracts/Srs.sol
+++ contracts/Srs.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.18;
 
 import "./ERC20.sol";
 
-
 contract Srs is ERC20 {
 
   constructor() ERC20("SRS", "Sirius", 18) {}
--- contracts/Zrc.sol
+++ contracts/Zrc.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import "./ERC20.sol";
 
-contract Zrc is ERC20, ERC20Detailed {
+contract Zrc is ERC20 {
 
   constructor() ERC20("ZRC", "Ziroconium", 18) {}
 
