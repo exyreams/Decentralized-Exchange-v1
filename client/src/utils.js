254364071f5b4f514eca5c461f866714a2cdc410
exyreams <exyreams@gmail.com>
2023-03-28 09:36:29 +0545

    #update328

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,31 +1,28 @@
 import Web3 from "web3";
 import ERC20Abi from "./ERC20Abi.json"
 import DecentralizedExchange from "./DecentralizedExchange.json";
-import detectEthereumProvider from "@metamask/detect-provider";
 
-const getWeb3 = () =>
-    new Promise(async (resolve, reject) => {
-        let provider = await detectEthereumProvider();
-        if (provider) {
-            await provider.request({ method: "eth_requestAccounts" });
-            try {
-                const web3 = new Web3(window.ethereum);
-                resolve(web3);
-            } catch (error) {
-                reject(error);
-            }
+const getWeb3 = async () => {
+    if (window.ethereum) {
+        try {
+            // Wait for the user to click the "Connect" button before requesting account access
+            await window.ethereum.enable();
+            return new Web3(window.ethereum);
+        } catch (error) {
+            throw new Error(error);
         }
-        reject("Install Metamask");
-    });
+    }
+    throw new Error("Install Metamask");
+};
 
-const getContracts = async web3 => {
+const getContracts = async (web3) => {
     const networkId = await web3.eth.net.getId();
     const deployedNetwork = DecentralizedExchange.networks[networkId];
     const decentralizedexchange = new web3.eth.Contract(
         DecentralizedExchange.abi,
         deployedNetwork && deployedNetwork.address,
     );
-    const tokens = await decentralizedexchange.methods.getTokens.call();
+    const tokens = await decentralizedexchange.methods.getTokens().call();
     const tokenContracts = tokens.reduce((acc, token) => ({
         ...acc,
         [web3.utils.hexToUtf8(token.ticker)]: new web3.eth.Contract(
@@ -34,6 +31,6 @@ const getContracts = async web3 => {
         )
     }), {});
     return { decentralizedexchange, ...tokenContracts };
-}
+};
 
-export { getWeb3, getContracts };                
+export { getWeb3, getContracts };
