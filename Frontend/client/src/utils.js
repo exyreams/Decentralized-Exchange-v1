b25cd03a153df2bcdd23ef1020485394476b143c
exyreams <exyreams@gmail.com>
2023-04-11 01:01:07 +0545

    redeployment of the code,

--- Frontend/client/src/utils.js
+++ Frontend/client/src/utils.js
@@ -4,26 +4,31 @@ import DecentralizedExchange from "./contracts/DecentralizedExchange.json";
 import ERC20Abi from "./ERC20Abi.json";
 
 const getWeb3 = () =>
-  new Promise(async (resolve, reject) => {
-    let provider = await detectEthereumProvider();
-    if (provider) {
-      await provider.request({ method: "eth_requestAccounts" });
+    new Promise(async (resolve, reject) => {
+      let provider;
       try {
-        const web3 = new Web3(window.ethereum);
-        resolve(web3);
+        provider = await detectEthereumProvider();
       } catch (error) {
         reject(error);
       }
-    }
-    reject("Install Metamask");
-  });
+      if (provider && provider !== 'undefined' && provider !== null) {
+        await provider.request({ method: "eth_requestAccounts" });
+        try {
+          const web3 = new Web3(provider);
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      }
+      reject("Install Metamask");
+    });
 
 const getContracts = async web3 => {
   const networkId = "5777";
   const deployedNetwork = DecentralizedExchange.networks[networkId];
   const decentralizedexchange = new web3.eth.Contract(
-    DecentralizedExchange.abi,
-    deployedNetwork && deployedNetwork.address,
+      DecentralizedExchange.abi,
+      deployedNetwork && deployedNetwork.address,
   );
   let tokens;
   try {
@@ -35,12 +40,11 @@ const getContracts = async web3 => {
   const tokenContracts = tokens.reduce((acc, token) => ({
     ...acc,
     [web3.utils.hexToUtf8(token.ticker)]: new web3.eth.Contract(
-      ERC20Abi,
-      token.tokenAddress
+        ERC20Abi,
+        token.tokenAddress
     )
   }), {});
   return { decentralizedexchange, ...tokenContracts };
 }
-
 
 export { getWeb3, getContracts };
